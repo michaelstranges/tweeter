a56bd8cf7d3195be5fa297e00cf0a93b76db3240
Michael Stranges <strangesm@gmail.com>
2018-03-30 19:39:06 +0000

    Implemented MongoDB communication to load tweers FROM database and send new tweets TO database

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,18 +9,28 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+     // simulateDelay(() => {
+        //db.tweets.push(newTweet); // IMPORTANT
+        db.collection("tweets").insertOne(newTweet);
         callback(null, true);
-      });
+      //});
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      //simulateDelay(() => {
+       // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+        //console.log(db.collection("tweets").find());
+
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if(err){
+          return callback(err);
+        }
+        callback(null, tweets);
+      })
+
+       // callback(null, db.tweets.sort(sortNewestFirst));
+      //});
     }
 
   };
